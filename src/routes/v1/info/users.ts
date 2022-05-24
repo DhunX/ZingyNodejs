6e2110e18e1f863dd1065fd2832f7bfe788ebbe6
Anand Jee Choubey <anandjeechoubey@Anands-MacBook-Air.local>
2022-05-24 20:28:56 +0530

    People Search

--- src/routes/v1/info/users.ts
+++ src/routes/v1/info/users.ts
@@ -17,6 +17,17 @@ export const getAllUsers = router.get('/', async (req, res) => {
     }
     return res.status(404).json({ message: 'User not found', status: 404 });
   }
+  if (data.q && typeof data.q === 'string') {
+    const q = data?.q;
+    const userByName = await UserModel.find({ name: { $regex: q } });
+    const userByUserName = await UserModel.find({ username: { $regex: q } });
+    const userByEmail = await UserModel.find({ email: { $regex: q } });
+    const user = [...userByName, ...userByUserName, ...userByEmail];
+    if (user && user.length > 0) {
+      return res.status(200).json({ status: 200, message: 'Users found', data: user, q: q });
+    }
+    return res.status(404).json({ message: 'No User found', status: 404 });
+  }
   try {
     const response = await UserModel.find();
     res.status(200).json({
